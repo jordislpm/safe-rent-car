ab009ae4ea1b6b0ed52d9ebc7b4efc35354819fa
jordislpm <jordislpm@gmail.com>
2024-05-13 18:34:32 -0400

    feat: project ready for start

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+
+export const metadata: Metadata = {
+  title: "Safe Rent Car",
+  description: "This is a perfect place for booking a car safe, quickly and easily",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`relative`}>
+        <Header/>
+        {children}
+        <Footer/>
+      </body>
+    </html>
+  );
+}
